fc065b8e16b9fce36d194689e7d8576937a05bca
Alex Shtokhov <133367282+alexshtokhov@users.noreply.github.com>
2023-08-03 14:55:35 +0300

    added last changes

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 let offset = 0;
 const sliderLine = document.querySelector('.service-slider-line');
 
@@ -10,6 +12,7 @@ document.querySelector('.slider-btn').addEventListener('click', function(){
 });
 
 
+
 let offset2 = 0;
 const sliderFeedbackLine = document.querySelector('.feedback-slider');
 
@@ -20,3 +23,31 @@ document.querySelector('.slider-feedback-btn').addEventListener('click', functio
     }
     sliderFeedbackLine.style.left = -offset2 + 'px';
 });
+
+
+
+
+let modal = document.getElementById("my-modal");
+let span = document.getElementsByClassName("close-modal-window")[0];
+
+let allBtn = document.querySelectorAll('.btn-modal-window');
+allBtn.forEach(function(element) {
+    element.onclick = showModal;
+})
+
+function showModal() {
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+  }
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+
+
